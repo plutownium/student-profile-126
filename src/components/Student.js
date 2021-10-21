bd75144969e20be9c4b312ca747e9957daed7d66
Roland Mackintosh <rolandmackintosh@outlook.com>
2021-10-21 05:54:46 -0700

    rough draft of calculating GPA from input

--- src/components/Student.js
+++ src/components/Student.js
@@ -3,6 +3,15 @@ import React from "react";
 import "./Students.css";
 
 function Student({ firstName, lastName, city, email, skill, grades, pic }) {
+  function calculateGPA(gradesInput) {
+    let calculatedGPA = 0;
+    for (let i = 0; i < gradesInput.length; i++) {
+      calculateGPA = calculateGPA + gradesInput[i];
+    }
+    calculateGPA = calculatedGPA / gradesInput.length;
+    return calculatedGPA;
+  }
+
   return (
     <div>
       <div className="studentContainer studentContainerHeightMod ">
@@ -15,12 +24,14 @@ function Student({ firstName, lastName, city, email, skill, grades, pic }) {
           <h3 className="namingPolicy">
             {firstName} {lastName}
           </h3>
-          <p>{city}</p>
-          <p>{email}</p>
-          <h4>
-            <p>{skill}</p>
-          </h4>
-          <ul>{grades}</ul>
+          <div className="studentDetails">
+            <p>{city}</p>
+            <p>{email}</p>
+            <h4>
+              <p>{skill}</p>
+            </h4>
+            <p>{calculateGPA(grades)}</p>
+          </div>
         </div>
       </div>
     </div>
